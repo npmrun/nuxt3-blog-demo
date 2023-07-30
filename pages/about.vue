c5c13225ead90ded4f64db91d90b16d8f64db762
npmrun <kk1549469775@gmail.com>
2023-07-30 23:28:12 +0800

    docs: add about

--- pages/about.vue
+++ pages/about.vue
@@ -1,16 +1,23 @@
 <script setup lang="ts">
+import gfm from "@bytemd/plugin-gfm";
+import frontmatter from "@bytemd/plugin-frontmatter";
+import btybreaks from "@bytemd/plugin-breaks";
+import zhHans from "bytemd/locales/zh_Hans.json";
+import { Viewer } from "@/components/MdEditor";
+import AboutRaw from "./about.md?raw"
 definePageMeta({
 	layout: "main-layout",
 });
 </script>
 <template>
 	<article class="prose lg:prose-xl m-10">
-		<h1></h1>
 		<p>
 			该系统为全栈项目，仅用于熟悉<code>nuxt3</code>，如遇到任何问题，可来此<a target="_blank"
 				href="https://github.com/npmrun/nuxt3-blog-demo">仓库</a>提issue！
 		</p>
 		<p>胸无点墨，不想卖弄。欢迎联系我。</p>
+		<Viewer :plugins="[gfm(), frontmatter(), btybreaks()]" :locale="zhHans" :value="AboutRaw">
+		</Viewer>
 	</article>
 </template>
 
